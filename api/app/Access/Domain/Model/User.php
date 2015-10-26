05b666f85961bf2edde606ff54ddc2c8cc473c95
Kenneth Söderlund <kenneth.soderlund@nordsoftware.com>
2015-10-26 15:56:16 +0200

    Add fixtures and services

---
+++ api/app/Access/Domain/Model/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soderluk
+ * Date: 26.10.2015
+ * Time: 9.40
+ */
+
+namespace Kkv\Access\Domain\Model;
+
+class User
+{
+
+}
---
+++ api/app/Common/Console/Kernel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soderluk
+ * Date: 26.10.2015
+ * Time: 9.24
+ */
+
+namespace Kkv\Common\Console;
+
+class Kernel
+{
+
+}
